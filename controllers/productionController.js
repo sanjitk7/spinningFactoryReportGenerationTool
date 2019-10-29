c0dc3466a20919591e21eb2287899849dc594ca6
Sanjit Kumar <sanjit2018@gmail.com>
2019-10-30 00:27:32 +0530

    added_buttons_for_all_queries_sidebar

--- controllers/productionController.js
+++ controllers/productionController.js
@@ -40,6 +40,120 @@ exports.production_report = function(req, res) {
 	}
 };
 
+//Display Idle workers
+exports.idleWorkerReport = function(req, res) {
+	console.log('inside idleWorker report');
+	if (req.query.startDate && req.query.startDate != '' && req.query.endDate && req.query.endDate != '') {
+		try {
+			console.log('inside idleWorker report');
+			const startDate = req.query.startDate;
+			const endDate = req.query.endDate;
+			const queryString =
+				"select distinct wid,fname,lname from workers where wid not in (select workers.wid from spinning_prod inner join workers on spinning_prod.wid=workers.wid where m_date between '" +
+				startDate +
+				"' and '" +
+				endDate +
+				"'group by wid )";
+			db.query(queryString, (err, rows, fields) => {
+				console.log(JSON.stringify(rows));
+				res.render('idleWorkerReport', { title: 'Idle Workers Report', productions: rows });
+			});
+		} catch (e) {
+			console.log('in err catch');
+			log.error(e);
+		}
+	} else {
+		res.sendStatus(400);
+		return;
+	}
+};
+
+//Display Machine Wise Production
+exports.machPdnReport = function(req, res) {
+	console.log('inside machPdn report');
+	if (req.query.startDate && req.query.startDate != '' && req.query.endDate && req.query.endDate != '') {
+		try {
+			console.log('inside machPdn report');
+			const startDate = req.query.startDate;
+			const endDate = req.query.endDate;
+			const queryString =
+				'select m_id,sum(m_prod)' +
+				" as machine_production ,round(sum(m_waste),2) as machine_waste from spinning_prod where m_date between '" +
+				startDate +
+				"' and '" +
+				endDate +
+				"'group by m_id";
+			db.query(queryString, (err, rows, fields) => {
+				console.log(JSON.stringify(rows));
+				res.render('machPdnReport', { title: 'Machine-wise Production Report', productions: rows });
+			});
+		} catch (e) {
+			console.log('in err catch');
+			log.error(e);
+		}
+	} else {
+		res.sendStatus(400);
+		return;
+	}
+};
+
+//Display Building Wise Data
+exports.bdnPdnReport = function(req, res) {
+	console.log('inside bdnPdn report');
+	if (req.query.startDate && req.query.startDate != '' && req.query.endDate && req.query.endDate != '') {
+		try {
+			console.log('inside bdnPdn report');
+			const startDate = req.query.startDate;
+			const endDate = req.query.endDate;
+			const queryString =
+				'select spinning_type.bid,sum(spinning_prod.m_prod) as building_prod from spinning_prod inner join spinning_type on spinning_prod.m_id=spinning_type.m_id where ' +
+				"m_date between '" +
+				startDate +
+				"' and '" +
+				endDate +
+				"'group by bid";
+			db.query(queryString, (err, rows, fields) => {
+				console.log(JSON.stringify(rows));
+				res.render('bdnPdnReport', { title: 'Building-wise Production Report', productions: rows });
+			});
+		} catch (e) {
+			console.log('in err catch');
+			log.error(e);
+		}
+	} else {
+		res.sendStatus(400);
+		return;
+	}
+};
+
+//Display Most used raw material
+exports.rawMatReport = function(req, res) {
+	console.log('inside rawMat report');
+	if (req.query.startDate && req.query.startDate != '' && req.query.endDate && req.query.endDate != '') {
+		try {
+			console.log('inside rawMat report');
+			const startDate = req.query.startDate;
+			const endDate = req.query.endDate;
+			const queryString =
+				"select item_no,item_name from raw_material_name where raw_material_name.item_no =(select t.item_code as maximum_used_amt from (select spinning_prod.item_no as item_code ,sum(raw_materials.qty) as item_qty_used from spinning_prod inner join raw_materials on spinning_prod.item_no=raw_materials.item_no and spinning_prod.lot_no=raw_materials.lot_no where m_date between '" +
+				startDate +
+				"' and '" +
+				endDate +
+				"' group by spinning_prod.item_no order by item_qty_used desc) as t limit 1)";
+			db.query(queryString, (err, rows, fields) => {
+				console.log(JSON.stringify(rows));
+				res.render('rawMatReport', { title: 'Maximum used Raw Material', productions: rows });
+			});
+		} catch (e) {
+			console.log('in err catch');
+			log.error(e);
+		}
+	} else {
+		res.sendStatus(400);
+		return;
+	}
+};
+
 //Display last 3 days of production report
 exports.production_report_last_3_days = function(req, res) {
 	try {
--- routes/production.js
+++ routes/production.js
@@ -11,7 +11,19 @@ router.get('/', production_controller.index);
 // GET request for production report.
 router.get('/reports', production_controller.production_report);
 
-// GET request for production report.
+//GET request for idle workers report
+router.get('/idleWorkerRep',production_controller.idleWorkerReport);
+
+//GET request for machine wise production report
+router.get('/machPdnRep',production_controller.machPdnReport);
+
+//GET request for building wise production report
+router.get('/bdnPdnRep',production_controller.bdnPdnReport);
+
+//GET request for  max consumer raw material report
+router.get('/rawMatRep',production_controller.rawMatReport);
+
+// GET request for worker wise production report.
 router.get('/reports/last_3_days', production_controller.production_report_last_3_days);
 
 // GET request for creating a production.
